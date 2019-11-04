8c435adeb5b594c5fcedb239b4fd432e93bfba9f
GeorgeOprian <49019556+GeorgeOprian@users.noreply.github.com>
2019-11-05 00:22:47 +0200

    Add files via upload

--- Test/test.py
+++ Test/test.py
@@ -4,6 +4,7 @@
 
 from itertools import chain, combinations
 import collections
+import random
 def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
@@ -24,42 +25,83 @@ def minMaxPositions (v, min, max):
     for i in range (len(v)):
         if min == v[i]:
             pozMin = i
-        if max = v[i]:
+        if max == v[i]:
             pozMax = i
     return (pozMin, pozMax)
 
-v = [7, 2, 4, 8, 3, 5, 9, 10, 1]
-numberOfSteps = 3
+n = random.randint(8, 12)
+v = random.sample(range(12), n)
+numberOfSteps = random.randint(2, n // 2)
 keep = []
 compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
 #asta e o functie lamnda, o folosesc ca sa gasesc sirurile generate in vectorul v
 
 for subset in powerset(v):
     if (len(subset) == numberOfSteps and sorted(subset)):
-        subset = list(subset)   
+        subset = list(subset)
         keep.append(subset)
+ok = False
     # pun in keep toate sirurile sortate si care sunt de lungima ceruta in cerinta
 subsets = []
 for i in range (len(v) - numberOfSteps):
     for subset in keep:
         if compare (v[i : i+len(subset)], subset) == True:
             subsets.append(subset)
-            #am spus mai sus de ce folosesc susets
-print(subsets)
+            ok = True
+                #am spus mai sus de ce folosesc subsets
+    #print(subsets)
 keep.clear()
+if ok == False:
+    print ("In vector nu exista o submultime ordonata de dimensiune", numberOfSteps)
+else:
+    subset = subsets[0]
+    minFirstSubset = subset[0]
+    maxFirstSubset = subset[numberOfSteps - 1]
+    print("\nSirul initial:", v)
+    print ("\nSubmultimea obtinuta dupa", numberOfSteps, "pasi prin algoritmul de sortare prin selectia minimului este", subset)
+    # stiu ca in subset o sa am sirul de numere sortat crescator
+    #pastrez prima si ultima valoare ca fiind mariginile intervalelor
 
-subset = subsets[0]
-minFirstSubset = subset[0]
-maxFirstSubset = subset[numberOfSteps - 1]
+    elemDeleted = []
+    sters = [0] * len(v)
+    (pozMin, pozMax) = minMaxPositions(v, minFirstSubset, maxFirstSubset)
 
-# stiu ca in suset o sa am sirul de numere sortat crescator
-#pastrez prima si ultima valoare ca fiind mariginile intervalelor
-
-elemDeleted = []
-(pozMin, pozMax) = minMaxPositions(v, minFirstSubset, maxFirstSubset)
-for i in range (len(v)):
-    if (v[i] > minFirstSubset and i < pozMin):
-        elemDeleted.append(v[i])
+    nrElemDeleted = 0
+    while (v[nrElemDeleted] != minFirstSubset): #sterge toate elementele care se afla inainte de minimul din secventa
+        elemDeleted.append(v[nrElemDeleted])
+        sters [nrElemDeleted] = 1
+        nrElemDeleted += 1
+    
+    #print(elemDeleted)
+    for i in range (nrElemDeleted + len(subset), len(v)): #aici sterg elementele care nu sunt in secventa si sunt mai mici decat minimul din secventa
+        if (v[i] < minFirstSubset and sters[i] != 1):
+            elemDeleted.append(v[i])
+            sters[i] = 1
+    #print(elemDeleted)
+    for i in range(nrElemDeleted + len(subset), len(v)):
+        if (len(elemDeleted) == numberOfSteps):
+            break
+        if (sters[i] != 1):
+            if (v[i] < minFirstSubset):
+                elemDeleted.append(v[i])
+                sters[i] = 1
+                print (v[i])
+                continue
+            if (v[i] > minFirstSubset and v[i] < maxFirstSubset):
+                elemDeleted.append(v[i])
+                sters[i] = 1
+                print(v[i])
+    print("\nNumarul minim de elemente pe care trebuie sa le stergem:", len(elemDeleted))
+    print ("\nElementele pe care le-am sters:", elemDeleted)
+#asta e prima abordare    
+# for i in range (len(v)):
+#     if (v[i] < minFirstSubset and i > pozMin):
+#         elemDeleted.append(v[i])
+#     if (v[i] > minFirstSubset and i < pozMin):
+#         elemDeleted.append(v[i])
+#     if (v[i] > minFirstSubset and v[i] < maxFirstSubset and i > pozMax):
+#         elemDeleted.append(v[i])
+    
     #am explicat in idei.txt ce vreau sa fac in forul asta, in principal asta e partea cea mai importanta din tema
     #
 
